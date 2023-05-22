77634b419adf058affba80fc31e47862464795c7
Millena Ferreira <22282227@CFP127ALUNO.LOCAL>
2023-05-22 16:54:44 -0300

    icone de data e horario

--- js/horario.js
+++ js/horario.js
@@ -1,6 +1,5 @@
 'use strict'
 
-console.log('arquivo carregado');
 
 // var pegandovalor = function (e) {
 //     myInput.value = e.target.value
@@ -11,14 +10,18 @@ console.log('arquivo carregado');
 
 
 export const btn = () => {
-    console.log('teste');
+
     const labelCalendar = document.getElementById("label_calendar");
     const myInput = document.getElementById("date_input");
+    const myInput2 = document.getElementById("time_input");
+    const labelTime = document.getElementById("label_time");
 
     labelCalendar.addEventListener('click', () => {
         myInput.showPicker();
-    
-     
+    })
+
+    labelTime.addEventListener('click', () => {
+        myInput2.showPicker();
     })
 }
 
